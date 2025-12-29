783d3b40ecbfca0964b1f5fe5e4102ebbeb194eb
robonen <robonenandrew@gmail.com>
2025-12-30 02:41:37 +0700

    feat(web): disable comments completely in vite output

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -4,9 +4,15 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
+  define: {
+    __VUE_OPTIONS_API__: false,
+  },
   build: {
     outDir: '../data',
     emptyOutDir: true,
+  },
+  esbuild: {
+    legalComments: 'none',
   },
   server: {
     port: 5173
